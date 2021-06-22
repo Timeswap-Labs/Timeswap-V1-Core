0d700ab8abac23c3545348c49fbf72f2840ac9b4
Varun Krishna S <varunhawk19@gmail.com>
2021-06-22 10:27:52 +0530

    adds README.md, prettier

--- contracts/interfaces/InterfaceERC721Receiver.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.8.1;
-
-interface InterfaceERC721Receiver {
-    // UPDATE
-
-    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data) external returns (bytes4);
-}
