ae064fa75ce275822bad3ee280c6e76bf22f57c6
Ricsson Ngo <mathepreneur.eth@gmail.com>
2021-08-09 11:04:55 +0500

    implement edit-pay

--- contracts/libraries/PayMath.sol
+++ contracts/libraries/PayMath.sol
@@ -6,19 +6,19 @@ import {SafeCast} from './SafeCast.sol';
 library PayMath {
     using SafeCast for uint256;
 
-    function getDebt(uint112 assetPay, uint112 debt) internal pure returns (uint112 debtIn) {
-        if (assetPay <= debt) return debtIn = assetPay;
-        debtIn = debt;
+    function getDebt(uint112 _debtIn, uint112 debt) internal pure returns (uint112 debtIn) {
+        if (_debtIn <= debt) return debtIn = _debtIn;
+        debtIn = _debtIn;
     }
 
     function getCollateral(
-        uint112 debtPay,
+        uint112 debtIn,
         uint112 collateral,
         uint112 debt
     ) internal pure returns (uint112 collateralOut) {
-        if (debtPay >= debt) return collateralOut = collateral;
+        if (debtIn >= debt) return collateralOut = collateral;
         uint256 _collateralOut = collateral;
-        _collateralOut *= debtPay;
+        _collateralOut *= debtIn;
         _collateralOut /= debt;
         collateralOut = _collateralOut.toUint112();
     }
