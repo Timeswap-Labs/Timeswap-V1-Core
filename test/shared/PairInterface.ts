dc8471a5cb141352da36ab0762a5cb9dca9848a5
Emmanuel Antony <emmanuelantony2000@gmail.com>
2021-08-13 19:10:03 +0530

    Implemented view functions

---
+++ test/shared/PairInterface.ts
@@ -0,0 +1,65 @@
+export interface Tokens {
+  asset: bigint
+  collateral: bigint
+}
+
+export function tokensDefault(): Tokens {
+  return { asset: 0n, collateral: 0n }
+}
+
+export interface Claims {
+  bond: bigint
+  insurance: bigint
+}
+
+export function claimsDefault(): Claims {
+  return { bond: 0n, insurance: 0n }
+}
+
+export interface Due {
+  debt: bigint
+  collateral: bigint
+  startBlock: bigint
+}
+
+export function dueDefault(): Due {
+  return { debt: 0n, collateral: 0n, startBlock: 0n }
+}
+
+export interface State {
+  asset: bigint
+  interest: bigint
+  cdp: bigint
+}
+
+export function stateDefault(): State {
+  return { asset: 0n, interest: 0n, cdp: 0n }
+}
+
+export interface Pool {
+  state: State
+  lock: Tokens
+  ownerLiquidity: bigint
+  senderLiquidity: bigint
+  totalLiquidity: bigint
+  totalClaims: Claims
+}
+
+export function poolDefault(): Pool {
+  return {
+    state: stateDefault(),
+    lock: tokensDefault(),
+    ownerLiquidity: 0n,
+    senderLiquidity: 0n,
+    totalLiquidity: 0n,
+    totalClaims: claimsDefault(),
+  }
+}
+
+export default {
+  tokensDefault,
+  claimsDefault,
+  dueDefault,
+  stateDefault,
+  poolDefault,
+}
