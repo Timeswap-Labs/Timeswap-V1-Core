30f721d8fbff1f892b82fc9457699f76d67c1498
varun krishna s <varunhawk19@gmail.com>
2021-09-05 17:44:07 +0530

    updates safeTransfer

--- contracts/libraries/SafeTransfer.sol
+++ contracts/libraries/SafeTransfer.sol
@@ -2,20 +2,16 @@
 pragma solidity =0.8.1;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {Contract} from './Contract.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 library SafeTransfer {
-    using Contract for address;
+    using SafeERC20 for IERC20;
 
     function safeTransfer(
         IERC20 token,
         address to,
         uint256 amount
     ) internal {
-        address(token).requireContract;
-        (bool success, bytes memory data) = address(token).call(
-            abi.encodeWithSelector(IERC20.transfer.selector, to, amount)
-        );
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TF');
+        token.safeTransfer(address(to), amount);
     }
 }
