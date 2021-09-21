c63a07ae1b787dd6541b1f9da99dd30b32a6782b
amateur_dev <me@dipeshsukhani.dev>
2021-09-21 15:22:27 +0800

    fix: WIP Getting forbidden error

--- test/pair/Pay.test.ts
+++ test/pair/Pay.test.ts
@@ -1,41 +1,78 @@
+import chai from 'chai'
 import { ethers, waffle } from 'hardhat'
 import { advanceTimeAndBlock, now } from '../shared/Helper'
 import testCases from './TestCases'
 import { expect } from '../shared/Expect'
 import { payFixture, constructorFixture, Fixture, mintFixture, borrowFixture, lendFixture } from '../shared/Fixtures'
 
-const { loadFixture } = waffle
+const { loadFixture, solidity } = waffle
+chai.use(solidity)
+//TODO: Check why chai's native assertion library isnt working and remove the helper function
+function checkBigIntEquality(x: bigint, y: bigint) {
+  expect(x.toString()).to.equal(y.toString());
+}
 
 describe('Pay', () => {
-  const tests = testCases.pay();
+  //TODO: to work on the payTests
+  // const payTests = testCases.pay();
+  const tests = {
+    Success: [
+      {
+        mintParams:
+        {
+          assetIn: 2000n,
+          collateralIn: 800n,
+          interestIncrease: 20n,
+          cdpIncrease: 400n
+        },
+        borrowParams:
+        {
+          assetOut: 200n,
+          collateralIn: 72n,
+          interestIncrease: 1n,
+          cdpIncrease: 2n
+        },
+        payParams:
+          { ids: [1n], debtIn: [200n], collateralOut: [72n] }
+      }
+    ],
+  };
 
   async function fixture(): Promise<Fixture> {
     const constructor = await constructorFixture(10000n, 10000n, (await now()) + 31536000n)
     return constructor
   }
 
+  // it('to be deleted', async () => {
+  //   console.log("PayTests:", tests);
+  // })
+
   // TODO: we are getting an object of mintCases, BorrowCases and payCases
-  // TODO: Need to restructur the testCases.pay to send in an array of the test cases
-  tests.Success.forEach((params, idx) => {
+  // TODO: Need to restructure the testCases.pay to send in an array of the test cases
+  tests.Success.forEach((test, idx) => {
     describe.only(`Success case ${idx + 1} for pay`, () => {
       async function fixtureSuccess(): Promise<Fixture> {
+        const { mintParams, borrowParams, payParams } = test
 
         const signers = await ethers.getSigners()
         const constructor = await loadFixture(fixture)
 
-        const mint = await mintFixture(constructor, signers[0], params.mintParams)
-        // neeed to add the lend params also // not necessary though but just to add more liquidity 
-        const borrow = await borrowFixture(mint, signers[0], params.borrowParams) // borrowing from the contract
+        const mint = await mintFixture(constructor, signers[0], mintParams)
+        const borrow = await borrowFixture(mint, signers[0], borrowParams)
 
-        await advanceTimeAndBlock(31536000)  // pushing the time ahead
-        const pay = await payFixture( // paying back the loan
+        await advanceTimeAndBlock(31535000)  // pushing the time ahead
+        const pay = await payFixture(
           borrow,
           signers[0],
-          params.payParams
+          payParams
         )
+        console.log(pay);
         return pay
       }
 
+      it('to be deleted', async () => {
+        const { pair, pairSim } = await loadFixture(fixtureSuccess)
+      })
 
       // it('Should have correct total reserves', async () => {
       //   const { pair, pairSim } = await loadFixture(fixtureSuccess)
@@ -43,8 +80,8 @@ describe('Pay', () => {
       //   const reserves = await pair.totalReserves()
       //   const reservesSim = pairSim.reserves
 
-        
-        
+
+
       //   expect(reserves.asset).to.equalBigInt(reservesSim.asset)
       //   expect(reserves.collateral).to.equalBigInt(reservesSim.collateral)
       // })
@@ -55,8 +92,8 @@ describe('Pay', () => {
       //   const state = await pair.state()
       //   const stateSim = pairSim.pool.state
 
-        
-        
+
+
 
       //   expect(state.asset).to.equalBigInt(stateSim.asset)
       // })
@@ -85,12 +122,13 @@ describe('Pay', () => {
 
       //   const claims = await pair.totalClaims()
       //   const claimsSim = pairSim.pool.totalClaims
-        
-        
+
+
 
       //   expect(claims.bond).to.equalBigInt(claimsSim.bond)
       //   expect(claims.insurance).to.equalBigInt(claimsSim.insurance)
       // })
     })
-  // })
-// })
+    // })
+  })
+})
