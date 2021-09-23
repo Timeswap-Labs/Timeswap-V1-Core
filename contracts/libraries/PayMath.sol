b82a4079448d140d803f0357a3d2e4fa8eeee883
amateur_dev <me@dipeshsukhani.dev>
2021-09-23 12:50:35 +0800

    logging parameters

--- contracts/libraries/PayMath.sol
+++ contracts/libraries/PayMath.sol
@@ -2,13 +2,19 @@
 pragma solidity =0.8.1;
 
 import {IPair} from '../interfaces/IPair.sol';
+import "hardhat/console.sol";
 
 library PayMath {
     function checkProportional(
         uint112 assetIn,
         uint112 collateralOut,
         IPair.Due memory due
-    ) internal pure {
+    ) internal view {
+        //TODO: to remove console.log and also make the function as pure
+        console.log("assetIn", assetIn);
+        console.log("collateralOut", collateralOut);
+        console.log("due.collateral", due.collateral);
+        console.log("due.debt", due.debt);
         require(uint256(assetIn) * due.collateral >= uint256(collateralOut) * due.debt, 'Forbidden');
     }
 }
