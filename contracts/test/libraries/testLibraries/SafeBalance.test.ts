96f2b3b7357c46fac705a59b46f5e9508cbab63a
ksatyarth2 <erkumar@protonmail.ch>
2021-10-08 15:54:23 +0530

    safeBalanceTest success

--- contracts/test/libraries/testLibraries/SafeBalance.test.ts
+++ contracts/test/libraries/testLibraries/SafeBalance.test.ts
@@ -1,14 +1,8 @@
 import chai from 'chai'
 import { ethers, waffle } from 'hardhat'
-import { Address } from 'hardhat-deploy/dist/types'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { constants } from 'ethers'
 import { SafeBalanceTest } from '../../../../typechain/SafeBalanceTest'
-
 import { IERC20 } from '../../../../typechain/IERC20'
-import type { TimeswapPair } from '../../../../typechain/TimeswapPair'
-import { IFactory } from '../../../../typechain/IFactory'
-
 import { testTokenNew } from '../../../../test/shared/TestToken'
 
 let signers: SignerWithAddress[]
@@ -20,18 +14,20 @@ const { expect } = chai
 describe('Checking SafeBalance', () => {
   let token: IERC20
   let safeBalTest: SafeBalanceTest
+  let tokenMinted = 1000n
+  let tokenTransfer = 600n
   before(async () => {
     signers = await ethers.getSigners()
   })
   beforeEach(async () => {
-    token = await testTokenNew('Ether', 'WETH', 1000n)
+    token = await testTokenNew('Ether', 'WETH', tokenMinted)
     const safeBal = await ethers.getContractFactory('SafeBalanceTest')
     safeBalTest = (await safeBal.deploy()) as SafeBalanceTest
     await safeBalTest.deployed()
-    //deploy safebalancetest.sol
-    //transfer x tokens to above address
+    token.transfer(safeBalTest.address, tokenTransfer)
   })
-  it('Returns safe balance', () => {
-    let safeBal = safeBalTest.safeBalance(token.address)
+  it('Should return the balance transferred', async () => {
+    let safeBalance = await safeBalTest.safeBalance(token.address)
+    expect(safeBalance).to.be.equal(tokenTransfer)
   })
 })
