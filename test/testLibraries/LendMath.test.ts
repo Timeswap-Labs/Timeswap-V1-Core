4e802aa4de35a2536d4138e2341ed8cd935519b9
ksatyarth2 <erkumar@protonmail.ch>
2021-10-12 11:01:09 +0530

    lendMath- need to add correct state parameter

--- test/testLibraries/LendMath.test.ts
+++ test/testLibraries/LendMath.test.ts
@@ -4,15 +4,30 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { LendMathTest } from '../../typechain/LendMathTest'
 import { IERC20 } from '../../typechain/IERC20'
 import { testTokenNew } from '../shared/TestToken'
+import { State } from '../shared/PairInterface'
 
 let signers: SignerWithAddress[]
 
 const { solidity } = waffle
 chai.use(solidity)
 const { expect } = chai
-
+interface StateParams {
+  asset: bigint
+  interest: bigint
+  cdp: bigint
+}
 describe('Testing LendMath', () => {
   let lendMathTestContract: LendMathTest
+  const state: StateParams = {
+    asset: 100n,
+    interest: 3n,
+    cdp: 2n,
+  }
+  const assetIn: bigint = 1000n //randomNumbers
+  const interestDecrease: bigint = 3n
+  const cdpDecrease: bigint = 2n
+  const fee: bigint = 2n
+
   before(async () => {
     signers = await ethers.getSigners()
   })
@@ -21,8 +36,12 @@ describe('Testing LendMath', () => {
     lendMathTestContract = (await LendMathTestContactFactory.deploy()) as LendMathTest
     await lendMathTestContract.deployed()
     //deploy the contract ; done
-    //get random parameters
+    //get random parameters ; done
     //pass parameters to contract and test library
     //compare both
+  })
+  it('should not revert for check', () => {
+    // need to get correct state parameter
+    lendMathTestContract.check(state, assetIn, interestDecrease, cdpDecrease, fee)
   })
 })
