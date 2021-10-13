497b4272a3b29dfa75aa0c65390df7b8463679a8
ksatyarth2 <erkumar@protonmail.ch>
2021-10-13 08:50:49 +0530

    check failing

--- test/testLibraries/LendMath.test.ts
+++ test/testLibraries/LendMath.test.ts
@@ -2,16 +2,36 @@ import chai from 'chai'
 import { ethers, waffle } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { LendMathTest } from '../../typechain/LendMathTest'
+import LendMath from '../libraries/LendMath'
 import { IERC20 } from '../../typechain/IERC20'
 import { testTokenNew } from '../shared/TestToken'
 import { State } from '../shared/PairInterface'
+import { BigNumber } from '@ethersproject/bignumber'
 
 let signers: SignerWithAddress[]
 
 const { solidity } = waffle
 chai.use(solidity)
 const { expect } = chai
+
+interface Token {
+  asset: bigint
+  collateral: bigint
+}
+interface Claims {
+  bond: bigint
+  insurance: bigint
+}
 interface StateParams {
+  reserves: Token
+  totalLiquidity: bigint
+  totalClaims: Claims
+  totalDebtCreated: bigint
+  x: bigint
+  y: bigint
+  z: bigint
+}
+interface StateTestParams {
   asset: bigint
   interest: bigint
   cdp: bigint
@@ -19,10 +39,20 @@ interface StateParams {
 describe('Testing LendMath', () => {
   let lendMathTestContract: LendMathTest
   const state: StateParams = {
-    asset: 100n,
-    interest: 3n,
-    cdp: 2n,
+    reserves: { asset: 100n, collateral: 100n },
+    totalLiquidity: 100n,
+    totalClaims: { bond: 100n, insurance: 100n },
+    totalDebtCreated: 100n,
+    x: 100n,
+    y: 100n,
+    z: 100n,
   }
+  const stateTest: StateTestParams = {
+    asset: 100n,
+    interest: 100n,
+    cdp: 100n,
+  }
+
   const assetIn: bigint = 1000n //randomNumbers
   const interestDecrease: bigint = 3n
   const cdpDecrease: bigint = 2n
@@ -42,6 +72,8 @@ describe('Testing LendMath', () => {
   })
   it('should not revert for check', () => {
     // need to get correct state parameter
-    lendMathTestContract.check(state, assetIn, interestDecrease, cdpDecrease, fee)
+    let lendMathContract = lendMathTestContract.check(state, assetIn, interestDecrease, cdpDecrease, fee)
+    let lendMathTest = LendMath.check(stateTest, assetIn, interestDecrease, cdpDecrease, fee)
+    expect(lendMathContract).to.be.equal(lendMathTest)
   })
 })
