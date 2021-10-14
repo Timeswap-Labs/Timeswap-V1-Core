f96b19520843ad046b7f1f03b1d9acb0127125ab
amateur_dev <me@dipeshsukhani.dev>
2021-10-14 10:37:15 +0800

    LendMathTest 100percent done

--- test/testLibraries/LendMath.test.ts
+++ test/testLibraries/LendMath.test.ts
@@ -56,23 +56,22 @@ const stateTest: StateTestParams = {
 
 let maturity: BigNumberish;
 
+const assetIn: bigint = 1000n
+const interestDecrease: bigint = 30n
+const cdpDecrease: bigint = 2n
+const fee: bigint = 2n
+
+
 describe('LendMath', () => {
-  const assetIn: bigint = 1000n
-  const interestDecrease: bigint = 30n
-  const cdpDecrease: bigint = 2n
-  const fee: bigint = 2n
 
   before(async () => {
     signers = await ethers.getSigners()
     maturity = await now() + 10000n;
-  })
-  
-  beforeEach(async () => {
     const LendMathTestContactFactory = await ethers.getContractFactory('LendMathTest')
     lendMathTestContract = (await LendMathTestContactFactory.deploy()) as LendMathTest
     await lendMathTestContract.deployed()
-
   })
+
   it('Check should return true', async () => {
     const returnValue1 = await lendMathTestContract.check(state, assetIn, interestDecrease, cdpDecrease, fee)
     let returnValue2 = await LendMath.check(stateTest, assetIn, interestDecrease, cdpDecrease, fee);
@@ -81,29 +80,25 @@ describe('LendMath', () => {
     expect(returnValue1).to.equal(returnValue2);
   })
 
-  it('GetBond should return bondOut', async () => {
+  it('GetBond should return the expected bondOut', async () => {
     const returnValue1 = await lendMathTestContract.getBond(maturity, assetIn, interestDecrease);
     let returnValue2 = await LendMath.getBond(BigInt(maturity.toString()), assetIn, interestDecrease, (await now()));
     expect(returnValue1).to.be.equalBigInt(returnValue2);
   })
-})
 
-describe('LendMath should fail', () => {
-  const assetIn: bigint = 1000n
-  const interestDecrease: bigint = 3n
-  const cdpDecrease: bigint = 2n
-  const fee: bigint = 2n
-
-  before(async () => {
-    signers = await ethers.getSigners()
-    maturity = await now() + 10000n;
+  it('GetInsurance should return the expected InsuranceOut', async () => {
+    const returnValue1 = await lendMathTestContract.getInsurance(maturity, state, assetIn, interestDecrease);
+    let returnValue2 = await LendMath.getInsurance(BigInt(maturity.toString()), stateTest, assetIn, interestDecrease, (await now()));
+    expect(returnValue1).to.be.equalBigInt(returnValue2);
   })
-  beforeEach(async () => {
+
+  it('Check should be reverted', async () => {
+    const interestDecrease: bigint = 3n
+    maturity = await now() + 10000n;
     const LendMathTestContactFactory = await ethers.getContractFactory('LendMathTest')
     lendMathTestContract = (await LendMathTestContactFactory.deploy()) as LendMathTest
-    await lendMathTestContract.deployed();
-  })
-  it('should revert with ', async () => {
+    await lendMathTestContract.deployed()
     await expect(lendMathTestContract.check(state, assetIn, interestDecrease, cdpDecrease, fee)).to.be.revertedWith("Minimum");
   })
+
 })
