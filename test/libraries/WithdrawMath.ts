8abdb42a33a75697b15e23cece42e59f53f6e117
amateur_dev <me@dipeshsukhani.dev>
2021-12-17 20:21:45 +0800

    Updated WithDrawMath.ts

--- test/libraries/WithdrawMath.ts
+++ test/libraries/WithdrawMath.ts
@@ -1,4 +1,3 @@
-import { mulDiv } from '../libraries/FullMath'
 import { State } from '../shared/PairInterface'
 
 export function getAsset(state: State, bondIn: bigint): bigint {
@@ -13,12 +12,22 @@ export function getAsset(state: State, bondIn: bigint): bigint {
 export function getCollateral(state: State, insuranceIn: bigint): bigint {
   let assetReserve = state.reserves.asset
   if (assetReserve >= state.totalClaims.bond) return 0n
-  let _collateralOut = state.totalClaims.bond
-  _collateralOut -= assetReserve
-  _collateralOut *= state.totalClaims.insurance
-  if (state.reserves.collateral * state.totalClaims.bond >= _collateralOut) return insuranceIn
-  _collateralOut = mulDiv(_collateralOut, insuranceIn, state.totalClaims.bond * state.totalClaims.insurance)
-  return _collateralOut
+  let deficit = state.totalClaims.bond;
+  deficit -= state.reserves.asset
+  if (
+    (state.reserves.collateral * state.totalClaims.bond) >= (deficit *state.totalClaims.insurance)
+  ) {
+    let _collateralOut = deficit;
+    _collateralOut *= insuranceIn;
+    _collateralOut /= state.totalClaims.bond;
+    return _collateralOut;
+  } else {
+    let _collateralOut = state.reserves.collateral;
+    _collateralOut *= insuranceIn;
+    _collateralOut /= state.totalClaims.insurance;
+    return _collateralOut
+  }
+  
 }
 
 export default {
