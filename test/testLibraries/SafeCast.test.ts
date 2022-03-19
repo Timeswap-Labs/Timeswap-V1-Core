3585676f6de83d5bcf08fd028f537ea282cded0d
amateur_dev <me@dipeshsukhani.dev>
2022-03-19 11:30:12 +0800

    refacroting safeCast

--- test/testLibraries/SafeCast.test.ts
+++ test/testLibraries/SafeCast.test.ts
@@ -8,6 +8,7 @@ const { solidity } = waffle
 chai.use(solidity)
 
 let safeCastTestContract: SafeCastTest
+const MaxUint111 = BigNumber.from(2).pow(111).sub(1)
 const MaxUint112 = BigNumber.from(2).pow(112).sub(1)
 const MaxUint113 = BigNumber.from(2).pow(113).sub(1)
 const MaxUint128 = BigNumber.from(2).pow(128).sub(1)
@@ -30,5 +31,6 @@ describe('safeCast', () => {
   })
   it('should truncate to Uint112', async () => {
     expect(await safeCastTestContract.truncateUint112(MaxUint113)).to.be.equal(MaxUint112)
+    expect(await safeCastTestContract.truncateUint112(MaxUint111)).to.be.equal(MaxUint111)
   })
 })
